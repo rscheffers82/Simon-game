81240105b4fb30f030a072d18001479c4b7268db
Roy Scheffers <r.scheffers@gmail.com>
2016-10-03 17:30:11 -0400

    Upgrade layout

--- main_script.js
+++ main_script.js
@@ -2,3 +2,22 @@
 $( document ).ready(function() {
 
 });
+
+window.addEventListener("resize", correctSize);
+
+function correctSize() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    //document.getElementById( document ).innerHTML = "Width: " + w + "<br>Height: " + h;
+	if (w > h) {
+		console.log('Width = larger');	
+		$('.simon-part-middle').css('top','24.35vh');
+  		$('.simon-part-middle').css('left','24.4vh');		
+	}
+	else {
+		console.log('height = larger');
+		// vh to vw change
+		$('.simon-part-middle').css('top','24.35vw');
+  		$('.simon-part-middle').css('left','24.4vw'); 
+	}
+}

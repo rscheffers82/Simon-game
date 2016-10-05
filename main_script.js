48a8c2711ea2789fea32d1b46f0314203fd17409
Roy Scheffers <r.scheffers@gmail.com>
2016-10-05 16:55:41 -0400

    Add resize fix, text stays at correct position, size needs to be adjusted

--- main_script.js
+++ main_script.js
@@ -9,25 +9,36 @@ function correctSize() {
     var w = window.innerWidth;
     var h = window.innerHeight;
     //document.getElementById( document ).innerHTML = "Width: " + w + "<br>Height: " + h;
-	//var width = 80vw;
-	//var height = 80vw;
-	//var maxHeight = 80vh;
-	//var maxWidth = 80vh;
+//	var width = 80vw;
+//	var height = 80vw;
+//	var maxHeight = 80vh;
+//	var maxWidth = 80vh;
 
 	css_values = [{
-		"name": ".simon-part-middle",
+		"name": ".count",
 		"property": "top",
-		"value": "24.35" 
-	},
-	{
-		"name": ".simon-part-middle",
-		"property": "left",
-		"value": "24.4"
+		"value": "45.5" 
 	},{
 
-		"name": "",
-		"property": "",
-		"value": "" 
+		"name": ".count",
+		"property": "left",
+		"value": "28.3"
+	},	{
+		"name": ".start-txt",
+		"property": "top",
+		"value": "45.5" 
+	},{
+		"name": ".start-txt",
+		"property": "left",
+		"value": "37.0" 
+	},{		
+		"name": ".strict-txt",
+		"property": "top",
+		"value": "45.5" 
+	},{
+		"name": ".strict-txt",
+		"property": "left",
+		"value": "44.5" 
 	}];
 
 	var add = '';
@@ -40,11 +51,13 @@ function correctSize() {
 		// vh to vw change
 		add = 'vw';
 	}
-	console.log('all values', css_values);
-	console.log('add', css_values[0].value + add)
-
-	$( css_values[0].name).css(css_values[0].property, css_values[0].value + add);
-	$( css_values[1].name).css(css_values[1].property, css_values[1].value + add);
+	//console.log('all values', css_values);
+//	console.log('add', css_values[0].value + add)
+	css_values.forEach(function(e,count){
+		$( css_values[count].name).css(css_values[count].property, css_values[count].value + add);
+	});
+	//$( css_values[0].name).css(css_values[0].property, css_values[0].value + add);
+	//$( css_values[1].name).css(css_values[1].property, css_values[1].value + add);
 }
 
 $('.simon').on('click', function(e){

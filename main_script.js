53ebe888332713eddd3fbec32133d1cd90562e4e
Roy Scheffers <r.scheffers@gmail.com>
2016-10-10 20:19:30 -0400

    Add working promise

--- main_script.js
+++ main_script.js
@@ -307,4 +307,57 @@ input:checked + .slider:before {
 //     Ensure the game is correctly displayed     \\
 $( document ).ready(function() {
 	correctSize();
+
+var promiseCount = 0;
+
+function testPromise() {
+    var thisPromiseCount = 11;
+
+    console.log('beforeend', thisPromiseCount +
+        ') Started (<small>Sync code started</small>)<br/>');
+
+    // We make a new promise: we promise a numeric count of this promise, starting from 1 (after waiting 3s)
+    var p1 = new Promise(
+        // The resolver function is called with the ability to resolve or
+        // reject the promise
+        function(resolve, reject) {
+            console.log('beforeend', thisPromiseCount +
+                ') Promise started (<small>Async code started</small>)<br/>');
+            // This is only an example to create asynchronism
+            window.setTimeout(
+                function() {
+                    // We fulfill the promise !
+                    resolve(20);
+                }, Math.random() * 2000 + 1000);
+        }
+    );
+
+    // We define what to do when the promise is resolved/fulfilled with the then() call,
+    // and the catch() method defines what to do if the promise is rejected.
+    p1.then(
+        // Log the fulfillment value
+        function(val) {
+            console.log('beforeend', val +
+                ') Promise fulfilled (<small>Async code terminated</small>)<br/>');
+            	return val + 10;
+        })
+        .then(
+    	function(val) {
+    		console.log(val);
+    	})
+    .catch(
+        // Log the rejection reason
+        function(reason) {
+            console.log('Handle rejected promise ('+reason+') here.');
+        });
+
+    console.log('beforeend', thisPromiseCount +
+        ') Promise made (<small>Sync code terminated</small>)<br/>');
+}
+
+testPromise();
+
+
+
+
 });
